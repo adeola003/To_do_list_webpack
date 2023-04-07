9379cbb1b55528d95ddb2c409cdbc97edf75a4df
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-07 15:00:49 +0100

    group all tests in describe

--- src/functions.test.js
+++ src/functions.test.js
@@ -7,33 +7,41 @@ global.TextDecoder = require('util').TextDecoder;
 // eslint-disable-next-line
 const { JSDOM } = require('jsdom');
 
-const tasksArray = [];
+describe("Task Manager", () => {
 
-test('stringLength returns the correct length of a string', () => {
-  // test code here
+
+    const tasksArray = [];
+
+    describe("Task description editing function", () => {
+        test('add function should add a new task to the tasks array', () => {
+            // Arrange
+            const newTaskDescription = 'New Task';
+            // Act
+            add(newTaskDescription);
+          
+            setTimeout(() => {
+              // Assert
+              expect(tasksArray[0].description).toBe(newTaskDescription);
+            }, 1000);
+          });
+        
+        });
+    
+        describe("Task description editing function", () => {
+        
+            test('removeTask function should remove a task from the tasks array', () => {
+                // Arrange
+                const idToRemove = 0;
+                const expectedLength = 0;
+              
+                // Act
+                removeTask(idToRemove);
+              
+                // Assert
+                expect(tasksArray.length).toBe(expectedLength);
+                expect(tasksArray.find((task) => task.id === idToRemove)).toBeUndefined();
+              });
+        });  
+
 });
 
-test('add function should add a new task to the tasks array', () => {
-  // Arrange
-  const newTaskDescription = 'New Task';
-  // Act
-  add(newTaskDescription);
-
-  setTimeout(() => {
-    // Assert
-    expect(tasksArray[0].description).toBe(newTaskDescription);
-  }, 1000);
-});
-
-test('removeTask function should remove a task from the tasks array', () => {
-  // Arrange
-  const idToRemove = 0;
-  const expectedLength = 0;
-
-  // Act
-  removeTask(idToRemove);
-
-  // Assert
-  expect(tasksArray.length).toBe(expectedLength);
-  expect(tasksArray.find((task) => task.id === idToRemove)).toBeUndefined();
-});

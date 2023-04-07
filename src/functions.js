c1fd855e40acfe07d410ca25609efc999f3900de
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-04-07 09:05:19 +0100

    install jest and jsdom environment

--- src/functions.js
+++ src/functions.js
@@ -96,9 +96,19 @@ const loadFromStorage = () => {
   }
 };
 
+const stringLength = (string) => {
+  if(string.length >= 1 && string.length <= 10){
+      return string.length
+  }
+  else {
+      throw new Error('String must be at least1 character long and not longer than 10')
+  }
+  
+};
+
 export {
   tasksContainer, userEntry,
   addTask, clearCompleted,
   updateStorage, loadFromStorage,
-  add, removeTask, displayTasks, toggleCompleted,
+  add, removeTask, displayTasks, toggleCompleted, stringLength
 };
